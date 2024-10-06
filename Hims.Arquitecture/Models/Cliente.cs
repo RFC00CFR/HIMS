dcba5ec86a84da185f561f1c2cffa0bcd74a5731
Roberto Fernández <98728350+RFC00CFR@users.noreply.github.com>
2024-10-05 18:33:48 -0600

    Add project files.

---
+++ Hims.Arquitecture/Models/Cliente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hims.Arquitecture.Models;
+
+public partial class Cliente
+{
+    public int ClienteId { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public string Dni { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string? Telefono { get; set; }
+
+    public string? Direccion { get; set; }
+
+    public string? CodigoPostal { get; set; }
+
+    public int NumeroCliente { get; set; }
+
+    public string? Genero { get; set; }
+
+    public string? Cumpleaños { get; set; }
+
+    public string? Grupos { get; set; }
+
+    public string? App { get; set; }
+
+    public string? CifNif { get; set; }
+
+    public string? RazonSocial { get; set; }
+}
