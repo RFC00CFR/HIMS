8dea89b13adcf6d90b3847efc07541dd0b490cb6
Lawrence <laww.bl19@gmail.com>
2024-10-09 21:58:45 -0600

    frontend de citas

--- Hims.Web/Controllers/HomeController.cs
+++ Hims.Web/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace Hims.Web.Controllers
             return View();
         }
 
+        public IActionResult Cita()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
