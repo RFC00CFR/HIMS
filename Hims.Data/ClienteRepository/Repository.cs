1f0fc7c74d1d000d606b0c066071cc6a6ded8a14
Roberto Fernández <98728350+RFC00CFR@users.noreply.github.com>
2024-10-10 21:39:23 -0600

    Modulo Admin (CRUD): Conexcion DB Models Repository Pattern Controlador y vistas
    Front end Admin

---
+++ Hims.Data/ClienteRepository/Repository.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Hims.Arquitecture.Models;
+
+namespace Hims.Data.ClienteRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        bool Add(T entity);
+        T Update(T entity);
+        bool Delete(int id);
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+    }
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly HimsContext _context;
+        private DbSet<T> _dbSet;
+
+        public Repository(HimsContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public bool Add(T entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool Delete(int id)
+        {
+            var entity = GetById(id);
+            if (entity == null)
+            {
+                return false;
+            }
+
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate).ToList();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetById(int id)
+        {
+            var entity = _dbSet.Find(id);
+            if (entity == null)
+            {
+                throw new KeyNotFoundException($"Entity with id {id} not found.");
+            }
+            return entity;
+        }
+
+        public T Update(T entity)
+        {
+            _dbSet.Update(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+    }
+}
--- Hims.Web/Program.cs
+++ Hims.Web/Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Hims.Arquitecture.Models;
+using Hims.Data.ClienteRepository;
+using TM.Data.ClienteRepository;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Connection String
+builder.Services.AddDbContext<HimsContext>(options
+    => options.UseSqlServer(builder.Configuration.GetConnectionString("Server=DESKTOP-PCB1OR7;Database=HIMS;Trusted_Connection=True;TrustServerCertificate=True;")));
+
+//EmpleadoRepository
+builder.Services.AddScoped<IClienteRepository, ClienteRepository>();
 
 var app = builder.Build();
 
